f860046891f9537cd79f018dd3b321e90b2543ba
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-26 18:10:48 +0200

    make build

---
+++ index.9b3b7062.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(e){var t=document.querySelector(".spider");e.target.closest(".wall")&&(t.style.top=e.offsetY-t.clientHeight/2+"px",t.style.left=e.offsetX-t.clientWidth/2+"px")});
+//# sourceMappingURL=index.9b3b7062.js.map
