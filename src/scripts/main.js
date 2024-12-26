4f1a10c1e928e8f3cdd35ae74777e550c76202b7
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-26 17:20:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const img = document.querySelector('.spider');
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  img.style.clientTop = e.clientY + img.clientHeight / 2;
+  img.style.clientLeft = e.clientX + img.clientWidth / 2;
 });
